1f9c3ab9fb2c9aa012702955a706d4bd1c452d9d
Argenis Oldair Palacios Escobar A.K.A. Dr. Lfnt <Dr.Lfnt@gmail.com>
2015-01-16 12:48:35 -0600

    Web Site
    Full a necesidades del cliente

---
+++ contacto/contacto_error.php
@@ -0,0 +1,62 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<title>Gracias por contactarnos</title> 
+<meta name="author" content="Argenis Palacios A.K.A. Dr. Lfnt">
+<link href="../css/orion.css" rel="stylesheet" type="text/css">
+<link rel="shortcut icon" type="image/x-icon" href="../imagenes/favicon.ico">
+
+</head>
+
+<body>
+<br />
+<div class="container">
+
+  <div class="header">
+  <h1>
+  Servicio Orion S.A. de C.V.
+  </h1>
+  </div>
+  
+  <div class="content">  
+  
+    <div class="graciaserror">
+    <h1 style="text-align:center; color:#FFF; font-size:18px;">
+  <a href="http://servicioorion.com.mx/">INICIO</a> &middot; <a href="http://servicioorion.com.mx/servicio-orion/index.html">SERVICIO ORION</a> &middot; <a href="http://servicioorion.com.mx/servicio-orion/index.html#nosotros">NOSOTROS</a> &middot; <a href="http://servicioorion.com.mx/servicio-orion/index.html#mision">MISIÓN</a> &middot; <a href="http://servicioorion.com.mx/servicio-orion/index.html#vision">VISIÓN</a> &middot; <a href="http://servicioorion.com.mx/servicio-orion/index.html#valores">VALORES</a> &middot; <a href="http://servicioorion.com.mx/contacto/index.html">CONTACTO</a><br />
+    </h1>
+    </div>
+    <br /><br /><br /><br />
+    <div class="corners">
+      <table width="950" border="0" cellspacing="0" cellpadding="0">
+   	    <tr>
+    	    <td>&nbsp;</td>
+    	    <td>
+            	<h1  style="font-size:200%; text-align:center; color:black; text-shadow: 1px 1px gray, 2px 2px gray, 3px 3px gray; -webkit-transition: all 0.12s ease-out; -moz-transition: all 0.12s ease-out; -o-transition: all 0.12s ease-out;">Error. Su Consulta no pudo ser enviada<br />debido a que:</h1>
+                <h1><?php echo $error; ?></h1>
+                <h1 style="font-size:200%; text-align:center; color:black; text-shadow: 1px 1px gray, 2px 2px gray, 3px 3px gray; -webkit-transition: all 0.12s ease-out; -moz-transition: all 0.12s ease-out; -o-transition: all 0.12s ease-out;">Haga <a href="javascript:history.back()">click aqu&iacute;</a> para volver al Formulario.</h1>
+            </td>
+    	    <td>&nbsp;</td>
+  	    </tr>
+  	  </table>
+    </div>    
+    
+  <!-- end .content -->
+  </div>
+  <br /><br /><br />
+  <div class="footer">
+  <h6>
+    CALIDAD &middot; SERVICIO &middot; HONESTIDAD &middot; CONFIABILIDAD
+  <span style="color:white;">
+  ______________________________________________________________________________________________________
+  </span>
+  <br /><br />
+  Servicio Orion S.A. de C.V. 2013
+  </h6>
+  <!-- end .footer --></div>
+  
+  <!-- end .container --></div>
+  
+</body>
+
+</html>
---
+++ contacto/contacto_gracias.php
@@ -0,0 +1,70 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<!-- Si Ud desea puede Eliminat el meta, la funcion de este es redireccionar a otra ruta despues de un tiempo de 5sg -->
+<meta http-equiv="refresh" content="15;URL=http://servicioorion.com.mx/">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<title>Gracias por contactarnos</title> 
+<meta name="author" content="Argenis Palacios A.K.A. Dr. Lfnt">
+<link href="../css/orion.css" rel="stylesheet" type="text/css">
+<link rel="shortcut icon" type="image/x-icon" href="../imagenes/favicon.ico">
+
+</head>
+
+<body>
+<br />
+<div class="container">
+
+  <div class="header">
+  <h1>
+  Servicio Orion S.A. de C.V.
+  </h1>
+  </div>
+  
+  <div class="content">  
+  
+    <div class="graciaserror">
+    <h1 style="text-align:center; color:#FFF; font-size:18px;">
+  <a href="http://servicioorion.com.mx/">INICIO</a> &middot; <a href="http://servicioorion.com.mx/servicio-orion/index.html">SERVICIO ORION</a> &middot; <a href="http://servicioorion.com.mx/servicio-orion/index.html#nosotros">NOSOTROS</a> &middot; <a href="http://servicioorion.com.mx/servicio-orion/index.html#mision">MISIÓN</a> &middot; <a href="http://servicioorion.com.mx/servicio-orion/index.html#vision">VISIÓN</a> &middot; <a href="http://servicioorion.com.mx/servicio-orion/index.html#valores">VALORES</a> &middot; <a href="http://servicioorion.com.mx/contacto/index.html">CONTACTO</a><br />
+    </h1>
+    </div>
+    <br /><br />
+    <div class="corners">
+      <table width="950" border="0" cellspacing="0" cellpadding="0">
+   	    <tr>
+    	    <td>&nbsp;</td>
+    	    <td>
+            	<h1 style="font-size:200%; text-align:center; color:black; text-shadow: 1px 1px gray, 2px 2px gray, 3px 3px gray; -webkit-transition: all 0.12s ease-out; -moz-transition: all 0.12s ease-out; -o-transition: all 0.12s ease-out;">
+                Su Consulta ha sido enviada satisfactoriamente, nos estaremos comunicando
+                <br />
+                lo mas pronto con Ud.
+                <br />
+                Lo invitamos a seguir visitando nuestra Página Web y apreciar los Servicios
+                <br />
+                que tenemos para Ud.
+                </h1>
+            </td>
+    	    <td>&nbsp;</td>
+  	    </tr>
+  	  </table>
+    </div>    
+    
+  <!-- end .content -->
+  </div>
+  <br />
+  <div class="footer">
+  <h6>
+    CALIDAD &middot; SERVICIO &middot; HONESTIDAD &middot; CONFIABILIDAD
+  <span style="color:white;">
+  ______________________________________________________________________________________________________
+  </span>
+  <br /><br />
+  Servicio Orion S.A. de C.V. 2013
+  </h6>
+  <!-- end .footer --></div>
+  
+  <!-- end .container --></div>
+  
+</body>
+
+</html>
